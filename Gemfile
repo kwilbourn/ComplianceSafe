210f7bec39e9817226cd97f4dafa9d1205008bc5
Kevin Wilbourn <kevinwilbourn@gmail.com>
2014-08-21 10:46:14 -0500

    Added Simple_Form, added Devise user views, Added document type selector to document forms

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@ source 'https://rubygems.org'
 gem 'devise'
 gem 'cancan'
 gem 'paperclip'
-
+gem 'simple_form'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.4'
 # Use sqlite3 as the database for Active Record
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140821151129) do
+ActiveRecord::Schema.define(version: 20140821153705) do
 
   create_table "areas", force: true do |t|
     t.string   "description"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20140821151129) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.integer  "document_type_id"
   end
 
   create_table "user_areas", force: true do |t|
