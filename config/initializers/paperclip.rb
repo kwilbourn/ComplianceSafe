1e4498dcacd557fd9afe49edce93f734675c48d3
Kevin Wilbourn <kevinwilbourn@gmail.com>
2014-10-13 08:15:50 -0500

    S3 Storage for files

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -4,4 +4,6 @@ module Paperclip
       false
     end
   end
+  Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+  Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
 end
