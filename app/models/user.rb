06b0a7e2d3a4ed08b1d16e4257f7a49d25fb95e6
Kevin wilbourn <kevinwilbourn@gmail.com>
2014-12-24 12:15:25 -0500

    Remove duplicate model info from user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :user_areas
-  has_many :areas, through: :user_areas
   has_many :documents
   has_settings do |s|
     s.key :alerts, :defaults=> { :days=> [60,30,15,10,5,4,3,2,1], :overdue=> true}
