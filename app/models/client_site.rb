49249bd5542138810727d039e26c8775d65b5278
Kevin wilbourn <kevinwilbourn@gmail.com>
2014-12-24 12:10:11 -0500

    Moved user_areas relationship to only affect sites.

--- app/models/client_site.rb
+++ app/models/client_site.rb
@@ -1,3 +1,5 @@
 class ClientSite < User
     belongs_to :account
+    has_many :user_areas
+    has_many :areas, through: :user_areas
 end
--- app/models/user_areas.rb
+++ app/models/user_areas.rb
@@ -1,4 +1,4 @@
 class UserAreas < ActiveRecord::Base
-  belongs_to :user
+  belongs_to :client_site
   belongs_to :area
 end
