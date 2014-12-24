04cba56f6ec51f563736608c9d02da4bd08fb630
Kevin wilbourn <kevinwilbourn@gmail.com>
2014-12-24 12:51:27 -0500

    Notes in client_manager, account creation; need to code up

--- app/models/client_manager.rb
+++ app/models/client_manager.rb
@@ -1,3 +1,12 @@
 class ClientManager < User
     belongs_to :account
+    ##This will be the only model with a trial mode.
+    ##New account creation will take data from 1 form (Name, Company, Email, Password) and create both ClientManager and Account models and associate the two.
+    ##Needs controller method to check if account exists. If not, needs to wizard user through account creation
+    #Before create find_account
+    #def find_account
+    #logic to find account info
+    #else, pop modal to create account
+    #end
+    
 end
