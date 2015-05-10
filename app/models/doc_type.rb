999c3104afcdfb1ea35fe30bb7383328fa294136
Kevin Wilbourn <kevinwilbourn@gmail.com>
2015-05-10 12:15:22 -0500

    Added presence validations to doc type

--- app/models/doc_type.rb
+++ app/models/doc_type.rb
@@ -4,4 +4,6 @@ class DocType < ActiveRecord::Base
    has_attached_file :sample_file
   validates_with AttachmentSizeValidator, :attributes => :sample_file, :less_than => 2.megabytes
   do_not_validate_attachment_file_type :sample_file 
+  validates :issuing_authority, presence: true
+  validates :description, presence: true
 end
--- spec/models/doc_type_spec.rb
+++ spec/models/doc_type_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe DocType do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    FactoryGirl.create(:doc_type).should be_valid
+  end
+  it "is invalid without a description" do
+    FactoryGirl.build(:doc_type, description: nil).should be_invalid
+  end
+    it "is invalid without an issuing authority" do
+    FactoryGirl.build(:doc_type, issuing_authority: nil).should be_invalid
+  end
 end
