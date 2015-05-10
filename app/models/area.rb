aa2850428f6b7f86deaec0c797eb1a5552baa37c
Kevin Wilbourn <kevinwilbourn@gmail.com>
2015-05-10 11:39:05 -0500

    Added presence validations to area spec

--- app/models/area.rb
+++ app/models/area.rb
@@ -2,4 +2,6 @@ class Area < ActiveRecord::Base
   has_many :user_areas
   has_many :client_sites, through: :user_areas
   has_many :doc_types
+  validates :description, presence: true
+  validates :identifier, presence: true
 end
--- spec/models/area_spec.rb
+++ spec/models/area_spec.rb
@@ -4,4 +4,10 @@ describe Area do
   it "has a valid factory" do
     FactoryGirl.create(:area).should be_valid
   end
+  it "is invalid without a description" do
+    FactoryGirl.build(:area, description: nil).should be_invalid
+  end
+  it "is invalid without an area identifier" do
+    FactoryGirl.build(:area, identifier: nil).should be_invalid
+  end
 end
