d3901a32aaf7ab54fb898a09ca89ddf03714265d
Kevin wilbourn <kevinwilbourn@gmail.com>
2015-07-03 18:14:07 -0400

    Migration Formatting

--- db/migrate/20150703214911_add_roles_attribute_to_clients.rb
+++ db/migrate/20150703214911_add_roles_attribute_to_clients.rb
@@ -1,6 +1,6 @@
 class AddRolesAttributeToClients < ActiveRecord::Migration
   def up
-        add_column :clients, :roles_mask, :integer
+    add_column :clients, :roles_mask, :integer
   end
   def down
     remove_column :clients, :roles_mask
