a70d0b7f4680ba9975e05c42f95da52290a06826
Kevin wilbourn <kevinwilbourn@gmail.com>
2015-07-03 17:52:27 -0400

    Added roles_mask attribute to client and defined four basic roles. Adjusted db schema to allow for roles to be set for clients.

---
+++ db/migrate/20150703214911_add_roles_attribute_to_clients.rb
@@ -0,0 +1,8 @@
+class AddRolesAttributeToClients < ActiveRecord::Migration
+  def up
+        add_column :clients, :roles_mask, :integer
+  end
+  def down
+    remove_column :clients, :roles_mask
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150629015810) do
+ActiveRecord::Schema.define(version: 20150703214911) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20150629015810) do
   create_table "clients", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "roles_mask"
   end
 
   create_table "doc_types", force: true do |t|
